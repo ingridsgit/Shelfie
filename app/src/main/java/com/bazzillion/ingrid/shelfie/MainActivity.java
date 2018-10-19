b40b9e097dada966da2f9d16c0246b3b324f9e76
Ingrid <ingrid.bazin31@gmail.com>
2018-10-19 13:26:12 -0700

    Implement MainActivity layout

--- app/src/main/java/com/bazzillion/ingrid/shelfie/MainActivity.java
+++ app/src/main/java/com/bazzillion/ingrid/shelfie/MainActivity.java
@@ -2,12 +2,21 @@ package com.bazzillion.ingrid.shelfie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView productListView;
+    private ArrayAdapter<String> productArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        productArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.product_array));
+        productListView = findViewById(R.id.product_list_view);
+        productListView.setAdapter(productArrayAdapter);
     }
 }
